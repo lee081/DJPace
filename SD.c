a718388cf53d1e959e897232ae3797d73ac6e61c
Sam Vercauteren <clay_animator@yahoo.com>
2015-04-24 19:05:39 -0700

    added serial port example code

---
+++ SD.c
@@ -0,0 +1,9 @@
+//
+//  SD.c
+//  final
+//
+//  Created by Sam Vercauteren on 4/24/15.
+//
+//
+
+#include "SD.h"
--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "InputInterrupts.h"
 #include "LCD.h"
 #include "Sound.h"
+#include "Serial.h"
 
 #define FOSC 9830400		// Clock frequency
 #define BAUD 9600              // Baud rate used by the LCD
@@ -12,19 +13,17 @@
 
 int main(void)
 {
-    //sequence();
+    sci_init();
     
+    sci_num(0xFF);
     
-    init_audio();
-    
-    /*
-     audio_data = 0xffff;
-     send_audio_data();*/
+    /*init_audio();
+
     send_audio_data(0x0001);
     send_audio_data(0xfffe);
     _delay_ms(5000);
     send_audio_data(0x0002);
-    send_audio_data(0xfffe);
+    send_audio_data(0xfffe);*/
     
     while(1);
     
@@ -42,18 +41,6 @@ int main(void)
     //{
         //display_bitmap();
         //color_bars();
-        //PORTB &= ~(1 << PB2);
-        //SPDR = 0x12;
-        //while(!(SPSR & (1<<SPIF)));  // wait until transmission is complete
-        //spiwrite(0x15);
-        //SPCR = temp_backup;
-        //PORTB |= (1 << PB2);
-        //writecommand(0x00);
-        //writedata(0x19);
-        
-        
-        
-        
     //}
     //InputInterruptsInit();
     //while(1);
